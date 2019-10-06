f3d9d145d8d26b930143937abb925794c139b022
Simon <sdujardin@agaetis.fr>
2019-10-06 16:43:45 +0200

    :sparkles: bullet

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -2,9 +2,9 @@ extends Area2D
 
 export (int) var speed = 150
 
-func _on_Visibility_screen_exited():
-    queue_free()
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
 
 func _process(delta):
-    var velocity = Vector2(speed ,0).rotated(rotation)
-    position += velocity * delta
+	var velocity = Vector2(speed ,0).rotated(rotation)
+	position += velocity * delta
--- Main.gd
+++ Main.gd
@@ -12,76 +12,69 @@ var success = false
 var level_finished = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    randomize()
-    $PulseTimer.wait_time = pulse_speed
-    $PulseTimeTimer.wait_time = pulse_speed / 4
-    var tween = get_node("Tween")
-    tween.interpolate_property(light, "modulate:a",
-            1 ,0.2, pulse_speed,
-            Tween.TRANS_SINE, Tween.EASE_OUT)
-    tween.start()
-    $PulseTimer.start()
-    
+	randomize()
+	$PulseTimer.wait_time = pulse_speed
+	$PulseTimeTimer.wait_time = pulse_speed / 4
+	var tween = get_node("Tween")
+	tween.interpolate_property(light, "modulate:a",
+			1 ,0.2, pulse_speed,
+			Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	tween.start()
+	$PulseTimer.start()
+	
 
 func _on_PulseTimer_timeout():
-    can_press = true
-    $PulseTimeTimer.start()
-    
-    # first bullet
-    # Choose a random location on Path2D.
-    $BulletPath/BulletSpawnLocation1.set_offset(randi())
-    # Create a Bullet instance and add it to the scene.
-    var bullet1 = Bullet.instance()
-    add_child(bullet1)
-    # Set the bullet's direction perpendicular to the path direction.
-    var direction1 = $BulletPath/BulletSpawnLocation1.rotation + PI / 2
-    # Set the bullet's position to a random location.
-    bullet1.position = $BulletPath/BulletSpawnLocation1.position
-    # Add some randomness to the direction.
-    direction1 += rand_range(-PI / 4, PI / 4)
-    bullet1.rotation = direction1
-    
-    # second bullet
-    # Choose a random location on Path2D.
-    $BulletPath/BulletSpawnLocation2.set_offset(randi())
-    # Create a Bullet instance and add it to the scene.
-    var bullet2 = Bullet.instance()
-    add_child(bullet2)
-    # Set the bullet's direction perpendicular to the path direction.
-    var direction2 = $BulletPath/BulletSpawnLocation2.rotation + PI / 2
-    # Set the bullet's position to a random location.
-    bullet2.position = $BulletPath/BulletSpawnLocation2.position
-    # Add some randomness to the direction.
-    direction2 += rand_range(-PI / 4, PI / 4)
-    bullet2.rotation = direction2
+	can_press = true
+	$PulseTimeTimer.start()
+	
+	for i in range(2):
+		# Choose a random location on Path2D.
+		$BulletPath/BulletSpawnLocation.set_offset(randi())
+		# Create a Bullet instance and add it to the scene.
+		var bullet = Bullet.instance()
+		add_child(bullet)
+		# Set the bullet's direction perpendicular to the path direction.
+		var direction = $BulletPath/BulletSpawnLocation.rotation + PI / 2
+		# Set the bullet's position to a random location.
+		bullet.position = $BulletPath/BulletSpawnLocation.position
+		bullet.connect("body_entered", self, "_on_Bullet_body_entered")
+		# Add some randomness to the direction.
+		direction += rand_range(-PI / 4, PI / 4)
+		bullet.rotation = direction
+
+func _on_Bullet_body_entered(body):
+	print(body.name)
+	if body.name == "Player":
+		var new_door_progress = $Door.door_progress - 1
+		if $Door.door_progress != 0:
+			$Door.change_door_progress(new_door_progress)
 
 func _on_PulseTimeTimer_timeout():
-    can_press = false
-    print(success)
-    if is_instance_valid($QTE) and success == false and $QTE.qte_progress != 0:
-        $QTE.reset_qte()
-    success = false
+	can_press = false
+	if is_instance_valid($QTE) and success == false and $QTE.qte_progress != 0:
+		$QTE.reset_qte()
+	success = false
 
-func _process(delta):
-    if can_press:
-        if is_instance_valid($QTE):
-            if $QTE.qte[$QTE.qte_progress] == "a" and Input.is_action_just_pressed("ui_a"):
-                qte_success()
-            elif $QTE.qte[$QTE.qte_progress] == "b" and Input.is_action_just_pressed("ui_b"):
-                qte_success()
-            elif $QTE.qte[$QTE.qte_progress] == "x" and Input.is_action_just_pressed("ui_x"):
-                qte_success()
-            elif $QTE.qte[$QTE.qte_progress] == "y" and Input.is_action_just_pressed("ui_y"):
-                qte_success()
-            elif $QTE.qte_progress != 0 and (Input.is_action_just_pressed("ui_a") or Input.is_action_just_pressed("ui_b") or Input.is_action_just_pressed("ui_x") or Input.is_action_just_pressed("ui_y")):
-                $QTE.reset_qte()
-            
+func _physics_process(delta):
+	if can_press:
+		if is_instance_valid($QTE):
+			if $QTE.qte[$QTE.qte_progress] == "a" and Input.is_action_just_pressed("ui_a"):
+				qte_success()
+			elif $QTE.qte[$QTE.qte_progress] == "b" and Input.is_action_just_pressed("ui_b"):
+				qte_success()
+			elif $QTE.qte[$QTE.qte_progress] == "x" and Input.is_action_just_pressed("ui_x"):
+				qte_success()
+			elif $QTE.qte[$QTE.qte_progress] == "y" and Input.is_action_just_pressed("ui_y"):
+				qte_success()
+			elif $QTE.qte_progress != 0 and (Input.is_action_just_pressed("ui_a") or Input.is_action_just_pressed("ui_b") or Input.is_action_just_pressed("ui_x") or Input.is_action_just_pressed("ui_y")):
+				$QTE.reset_qte()
+			
 func qte_success():
-    success = true
-    $QTE.qte_progress += 1
-    if $QTE.qte_progress == 4:
-        level_end()
-        
+	success = true
+	$QTE.qte_progress += 1
+	if $QTE.qte_progress == 4:
+		level_end()
+		
 func level_end():
 	var new_door_progress = $Door.door_progress + 1
 	$Door.change_door_progress(new_door_progress)
