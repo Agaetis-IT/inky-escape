89b6c8f532064c4d08c170271256641501f99bc6
Julien Usson <jusson@agaetis.fr>
2019-10-06 13:16:43 +0200

    :sparkles: bullet is an area2D

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -1,6 +1,10 @@
-extends RigidBody2D
+extends Area2D
 
-export var speed = 150
+export (int) var speed = 150
 
 func _on_Visibility_screen_exited():
     queue_free()
+
+func _process(delta):
+    var velocity = Vector2(1 ,0)
+    position += velocity * speed * delta
--- Turret/BulletGenerator.gd
+++ Turret/BulletGenerator.gd
@@ -5,9 +5,6 @@ export (PackedScene) var Bullet
 func _on_SpawnTimer_timeout():
     # Create a Bullet instance and add it to the scene.
     var bullet = Bullet.instance()
-    add_child(bullet)
     bullet.position = position
     bullet.rotation = rotation
-    # Set the velocity (speed & direction).
-    bullet.linear_velocity = Vector2(bullet.speed, 0)
-    bullet.linear_velocity = bullet.linear_velocity.rotated(rotation)
+    add_child(bullet)
